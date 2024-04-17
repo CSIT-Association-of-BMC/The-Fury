f33c9f3e937019986d45a0fed4f9ed7fe0501fcf
Amrit Pokharel <pokharelamrit16@gmail.com>
2024-04-17 20:38:38 +0545

    updates on testimonials and home page

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./_components/Hero";
 import Testimonials from "./_components/Testimonials";
+import Clients from "./_components/Clients";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <div className="background absolute top-0 left-0 w-full h-full -z-10"></div>
+      <Clients />
       <Testimonials />
     </>
   );
